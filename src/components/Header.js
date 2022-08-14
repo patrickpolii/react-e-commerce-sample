0b1bffd9b4067a27039572490d0166a599d79780
patrickpolii <poliipatrick07@gmail.com>
2022-08-15 01:27:21 +0700

    add login and product

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header() {
           <a href="products.html">Products</a>
           <a href="#about">About Us</a>
           <a href="#contact">Contact Us</a>
-          <a href="login.html" className="btn">
+          <a href="Login" className="btn">
             Login
           </a>
         </nav>
